a573bb3f6c96b3ed781c851ab695a86687282364
ykerblat <82052220+ykerblat@users.noreply.github.com>
2021-09-22 07:46:29 -0600

    Add files via upload

--- Week03/wa03.js
+++ Week03/wa03.js
@@ -1,6 +1,11 @@
+//NPM npm install async dotenv querystring
+//npm install path
+
 "use strict";
 
 // List of dependencies that will be mobilized for this task
+
+
 const fs = require('fs'),
       querystring = require('querystring'),
       got = require('got'),
@@ -16,30 +21,28 @@ const API_URL = 'https://geoservices.tamu.edu/Services/Geocode/WebService/Geocod
 
 // geocode addresses
 let meetingsData = [];
-// let addresses = /global streetAddress/; And fixing the data a little bit by removing uneccesary lines using the .split
+
+// First, I use the week03.js file to call the /AddressOnly.json from Week 02 and directly parsed it. I also make use of path.join in order to return a normalized path by merging two paths together, combined with some light data cleaning by using the .split metho.
 const dataDir = path.join(__dirname + '/AddressOnly.json') ;
 var addresses = JSON.parse(fs.readFileSync(dataDir)).split('\n');
 
 console.log(addresses);
 
-// Here I need to create a variable which are the adresses in an array
-
-
-// // eachSeries in the async module iterates over an array and operates on each item in the array in series
-async.eachSeries(addresses, function(value, callback) {
-    let query = {
-        streetAddress: value,
-        city: "New York",
-        state: "NY",
-        apikey: API_KEY,
-        format: "json",
-        version: "4.01"
-    };
+// // The eachSeries in the async module is used here to iterate over an array and operates on each item in the array in series
+    async.eachSeries(addresses, function(value, callback) {
+        let query = {
+            streetAddress: value,
+            city: "New York",
+            state: "NY",
+            apikey: API_KEY,
+            format: "json",
+            version: "4.01"
+        };
     
-//     // construct a querystring from the `query` object's values and append it to the api URL
+// construct a querystring from the `query` object's values and append it to the api URL
     let apiRequest = API_URL + '?' + querystring.stringify(query);
     console.log(apiRequest);
-
+    
     (async () => {
     	try {
     		const response = await got(apiRequest);
@@ -60,13 +63,16 @@ async.eachSeries(addresses, function(value, callback) {
 });
 
 
+//From there, I used fs.readFileSync to read the recently created JSON file that held all of the json data with the georeferenced data
+
 var rawData = JSON.parse(fs.readFileSync('Week03/first.json'));
 
-var finalData = []
- //create a loop to loop through all addresses
+var finalData = [];
+
+ //Here, I create a loop to loop through all addresses that were read.
 for (var i=0; i<rawData.length; i++) {
 
-//create new object to pass only the needed information (address, latitude, longitude)
+//Here, I create a new object to pass only the strictly required information (address, latitude, longitude)
 var tamuGeoIngredient = {
             address: rawData[i].InputAddress.StreetAddress,
             latLong: {
@@ -74,12 +80,12 @@ var tamuGeoIngredient = {
                 lng: rawData[i].OutputGeocodes[0].OutputGeocode.Longitude
             }};
 
-// push my new objects 
+// I then push my new objects.
 finalData.push(tamuGeoIngredient);
 
 }
 
 console.log(tamuGeoIngredient);
 
-//create a new file with just the necessary geocode information
+//I create a new file with just the necessary geocode information that was required for this week's assignment and ensure that JSON data is readable and adequately structured.
 fs.writeFileSync('PreciseLocation.json', JSON.stringify(finalData));
